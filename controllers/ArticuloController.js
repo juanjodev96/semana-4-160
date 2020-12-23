f73ef475e201e951b441dc212521cfccda91adda
juan josé <juanjosetrading@gmail.com>
2020-12-23 01:28:02 -0500

    controladores

---
+++ controllers/ArticuloController.js
@@ -0,0 +1,177 @@
+//import models from '../models';
+const models = require('../models');
+const Categoria = require('../models').Categoria;
+
+module.exports = {
+    add: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.create(req.body);
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    query: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.findOne({
+                    id: req.query.id
+                })
+                .populate('categoria', {
+                    nombre: 1
+                });
+            if (!reg) {
+                res.status(404).send({
+                    message: 'El registro no existe'
+                });
+            } else {
+                res.status(200).json(reg);
+            }
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    queryCodigo: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.findOne({
+                    codigo: req.query.codigo
+                })
+                .populate('categoria', {
+                    nombre: 1
+                });
+            if (!reg) {
+                res.status(404).send({
+                    message: 'El registro no existe'
+                });
+            } else {
+                res.status(200).json(reg);
+            }
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    list: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.findAll({
+                include: [{
+                    model: Categoria,
+                    as: 'categoria'
+                }],
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    listByCategorias: async (req, res, next) => {
+        try {
+            let result = [];
+            
+            const categorias = await models.Categoria.findAll({
+                attributes: ['id', 'nombre', 'descripcion', 'estado']
+            });
+            
+            for (const item of categorias) {
+                let temp = await models.Articulo.findAll({
+                    
+                    where: {
+                        categoriaId: item.id
+                    }
+                })
+                
+                result.push({
+                    nombre: item.nombre,
+                    categoriaId: item.id,
+                    descripcion: item.descripcion,
+                    estado: item.estado,
+                    articulos: temp
+
+                })
+            }
+           
+            res.status(200).json(result);
+
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+
+    update: async (req, res, next) => {
+        try {
+
+            const reg = await models.Articulo.update(req.body, {
+                where: {
+                    id: req.body.id
+                }
+            });
+
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    remove: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.findByIdAndDelete({
+                id: req.body.id
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    activate: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.update({
+                estado: 1
+            }, {
+                where: {
+                    id: req.body.id
+                }
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    },
+    deactivate: async (req, res, next) => {
+        try {
+            const reg = await models.Articulo.update({
+                estado: 0
+            }, {
+                where: {
+                    id: req.body.id
+                }
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Ocurrió un error'
+            });
+            next(e);
+        }
+    }
+}
---
+++ controllers/UsuarioController.js
@@ -0,0 +1,212 @@
+var bcrypt = require('bcryptjs');
+const models = require('../models');
+const token = require('../services/token');
+const allPermisos = require('../config/permisos');
+
+
+module.exports = {
+    login: async (req, res, next) => {
+        try {
+            let user = await models.Usuario.findOne({
+                where: {
+                    email: req.body.email
+                }
+            });
+
+            if (user) {
+                let match = await bcrypt.compare(req.body.password, user.password);
+
+                if (match) {
+                    let permisos = allPermisos[user.rol]
+                    let tokenReturn = await token.encode(user.id, permisos);
+
+                    res.status(200).json({
+                        user,
+                        tokenReturn
+                    });
+                } else {
+                    res.status(401).send({
+                        auth: false,
+                        accessToken: null,
+                        reason: "Invalid Password!"
+                    });
+                }
+            } else {
+                res.status(404).send({
+                    message: 'User Not Found.'
+                });
+            }
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+    add: async (req, res, next) => {
+        try {
+            req.body.password = await bcrypt.hash(req.body.password, 10);
+            /* console.log(req.body); */
+            const reg = await models.Usuario.create(req.body);
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+    query: async (req, res, next) => {
+        try {
+            const reg = await models.Usuario.findOne({
+                id: req.query.id
+            });
+            if (!reg) {
+                res.status(404).send({
+                    message: 'User Not Found.'
+                });
+            } else {
+                res.status(200).json(reg);
+            }
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+    list: async (req, res, next) => {
+        try {
+            let valor = req.query.valor;
+            const reg = await models.Usuario.findAll();
+            res.status(200).json(reg);
+
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+
+    
+    listTypeRoles: async (req, res, next) => {
+        try {
+
+            res.status(200).send([
+                'Administrador',
+                'Almacenero',
+                'Vendedor',
+            ]);
+
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+    listTypeDocumento: async (req, res, next) => {
+        try {
+
+            res.status(200).send([
+                'Cédula de ciudadanía',
+                'Tarjeta de identidad',
+                'Cédula de extranjería',
+                'Pasaport',
+                'Registro Civil'
+            ]);
+
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+    update: async (req, res, next) => {
+        try {
+            let pas = req.body.password;
+            const reg0 = await models.Usuario.findOne({
+                where: {
+                    id: req.body.id
+                }
+            });
+            if (pas != reg0.password) {
+                req.body.password = await bcrypt.hash(req.body.password, 10);
+            }
+            const reg = await models.Usuario.update(req.body, {
+                where: {
+                    id: req.body.id
+                }
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+    remove: async (req, res, next) => {
+        try {
+            const reg = await models.Usuario.findByIdAndDelete({
+                id: req.body.id
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+    activate: async (req, res, next) => {
+        try {
+            const reg = await models.Usuario.update({
+                estado: 1
+            }, {
+                where: {
+                    id: req.body.id
+                }
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+    deactivate: async (req, res, next) => {
+        try {
+            const reg = await models.Usuario.update({
+                estado: 0
+            }, {
+                where: {
+                    id: req.body.id
+                }
+            });
+            res.status(200).json(reg);
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    },
+
+    describe: async (req, res, next) => {
+        try {
+            let valor = req.query.valor;
+            const reg = await models.Usuario.describe();
+            res.status(200).json(reg);
+
+        } catch (e) {
+            res.status(500).send({
+                message: 'Error -> ' + e
+            });
+            next(e);
+        }
+    }
+}
